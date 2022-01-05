a99266cb396dad47ecb6da38bef435ced55344d2
chifuyus <97142051+chifuyus@users.noreply.github.com>
2022-01-04 23:53:22 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
+@@ -1,4 +1,5 @@
 var btnno = document.getElementById('btnno');
 var btnyes = document.getElementById('btnyes');
 
 btnno.addEventListener('mouseenter', () => {
     console.log("mouse entrando");
-    var pos = coordenadas();
-    btnno.style.top = `${pos.posy}px`;
-    btnno.style.left = `${pos.posx}px`;
+@@ -8,9 +9,13 @@ btnno.addEventListener('mouseenter', () => {
 })
 
 var coordenadas = () => {
+    var ancho = screen.width - 30;
     var ancho = screen.width - 100;
     var alto = screen.height - 200;
     var posx = Math.random() * (ancho - 0) + 0;
     var posy = Math.random() * (alto - 0) + 0;
     return { posx, posy }
+} 
 }
 
 btnyes.addEventListener('click', () => {
-    alert('te amo juli felis día 1 de novios :3');
-});
+    alert('te amo juli felis 1 día de novios :3');
